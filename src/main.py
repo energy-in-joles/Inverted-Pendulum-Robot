cde83d3264eee3598499033cecbd0604a2b35069
energy-in-joles <nzmjoel@gmail.com>
2024-08-26 22:55:18 +0800

    [STABLE] Implemented tensorboard logging callback and refactored code

--- src/main.py
+++ src/main.py
@@ -10,15 +10,11 @@ from run_model import train_model, eval_model
 def main(cfg: DictConfig):
     SRC_PATH = os.path.dirname(__file__)
     MODEL_DIR_PATH = os.path.join(
-        os.path.split(SRC_PATH)[0], cfg.model_dir_name, cfg.mode.model.name
+        os.path.split(SRC_PATH)[0], cfg.model_dir_name, cfg.model.name
     )
-
     os.makedirs(MODEL_DIR_PATH, exist_ok=True)
 
     model_file_path = os.path.join(MODEL_DIR_PATH, cfg.model_filename)
-
-    LOGS_DIR_PATH = os.path.join(MODEL_DIR_PATH, cfg.log_dir_name)
-    os.makedirs(LOGS_DIR_PATH, exist_ok=True)
 
     ser = Serial(
         port=cfg.serial.port, baudrate=cfg.serial.baudrate, timeout=cfg.serial.timeout
@@ -27,7 +23,12 @@ def main(cfg: DictConfig):
 
     if cfg.mode.name == "train":
         new_model_file_path = os.path.join(MODEL_DIR_PATH, cfg.mode.new_model_filename)
-        train_model(cfg, ser, model_file_path, new_model_file_path)
+
+        # only relevant if logging.enabled is True in config.yaml
+        LOGS_DIR_PATH = os.path.join(MODEL_DIR_PATH, cfg.logging.dir_name)
+        os.makedirs(LOGS_DIR_PATH, exist_ok=True)
+
+        train_model(cfg, ser, model_file_path, LOGS_DIR_PATH, new_model_file_path)
     else:
         eval_model(cfg, ser, model_file_path)
 
--- src/run_model.py
+++ src/run_model.py
@@ -1,20 +1,18 @@
 from serial import Serial
+import os
 from omegaconf import DictConfig
 from pendulum_env import PendulumEnv
-from random import uniform
-import torch
-import numpy as np
 from stable_baselines3 import PPO, SAC
-import logging
-from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv
+from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.logger import configure
 
 
 def eval_model(cfg: DictConfig, ser: Serial, model_file_path: str):
     env = PendulumEnv(cfg, ser)
     vec_env = DummyVecEnv([lambda: env])
 
-    if cfg.mode.model.name == "PPO":
+    if cfg.model.name == "PPO":
         model = PPO.load(model_file_path, device="cpu")
     else:
         model = SAC.load(model_file_path, device="cpu")
@@ -28,51 +26,39 @@ def eval_model(cfg: DictConfig, ser: Serial, model_file_path: str):
             state, reward, done, info = vec_env.step(action)
         state = vec_env.reset()
         done = False
-        # if keyboard interrupt
-        # env.close()
-
-
-# def train_model(cfg: DictConfig, ser: Serial, model_dir_path: str, model_file_name: str):
-#     iterations = 10000
-#     pendulumEnv = PendulumEnv(cfg, ser)
-#     print(pendulumEnv.episode_frame, iterations)
-#     while pendulumEnv.episode_frame < iterations:
-#         print(pendulumEnv.episode_frame)
-#         time.sleep(cfg.serial.sleep_t)
-#         action = uniform(-2, 2)
-#         action = 0
-#         observation, reward, terminated, truncated, info = pendulumEnv.step(np.array([action]))
-#         print(action, observation, reward, terminated, truncated, info)
-#         done = terminated or truncated
-#         if done:
-#             pendulumEnv.reset()
 
 
 def train_model(
     cfg: DictConfig,
     ser: Serial,
     current_model_file_path: str,
-    new_model_file_path: str = ""
+    log_dir: str,
+    new_model_file_path: str = "",
 ):
-    # logging.basicConfig(filename='ppo_training.log', level=logging.INFO)
-    # # Custom callback to log reward values
-    # class CustomCallback(BaseCallback):
-    #     def __init__(self, verbose=0):
-    #         super(CustomCallback, self).__init__(verbose)
+    # override tensorboard logging
+    class TensorboardLogging(BaseCallback):
+        def __init__(self, log_dir, run_name, verbose=0):
+            super(TensorboardLogging, self).__init__(verbose)
+            self.log_run_path = os.path.join(log_dir, run_name)
 
-    #     def _on_step(self) -> bool:
-    #         env = self.locals.get('env')
-    #         if env is not None:
-    #             reward = self.locals.get('rewards')
-    #             obs = env.get_attr('last_pos_info')[0]
-    #             logging.info(f"observation: {obs}, {reward}")
-    #         return True
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        def _on_training_start(self) -> None:
+            tensorboard_logger = configure(self.log_run_path, ["stdout", "tensorboard"])
+            self.model.set_logger(tensorboard_logger)
+
+        def _on_step(self) -> bool:
+            return True
+
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     env = PendulumEnv(cfg, ser)
-    model_cfg = cfg.mode.model
+    model_cfg = cfg.model
     if cfg.mode.from_scratch:
         if model_cfg.name == "PPO":
-            model = PPO(model_cfg.policy, env, verbose=model_cfg.verbose, device=device)
+            model = PPO(
+                model_cfg.policy,
+                env,
+                verbose=model_cfg.verbose,
+                device="cpu",
+            )
         else:
             model = SAC(
                 model_cfg.policy,
@@ -82,20 +68,22 @@ def train_model(
                 sde_sample_freq=model_cfg.sde_sample_freq,
                 use_sde_at_warmup=model_cfg.use_sde_at_warmup,
                 learning_starts=model_cfg.learning_starts,
-                device="cpu"
-                tensorboard_log=cfg.log_dir_name,
+                device="cpu",
             )
     else:
         if model_cfg.name == "PPO":
-            model = PPO.load(current_model_file_path, env=env, device=device)
+            model = PPO.load(current_model_file_path, env=env, device="cpu")
         else:
             model = SAC.load(current_model_file_path, env=env, device="cpu")
 
-    print(f"To visualize training in TensorBoard, run: tensorboard --logdir {LOGS_DIR_PATH}, then go to http://localhost:6006/ on browser")
-    
-    model.learn(
-        total_timesteps=cfg.mode.total_timesteps, tb_log_name=cfg.log_run_name
-    )  # , callback=CustomCallback())
+    if cfg.logging.enabled:
+        print(
+            f'To visualize training logs, run: tensorboard --logdir="{log_dir}", then go to http://localhost:6006/ on browser.'
+        )
+        callback = TensorboardLogging(log_dir=log_dir, run_name=cfg.logging.run_name)
+        model.learn(total_timesteps=cfg.mode.total_timesteps, callback=callback)
+    else:
+        model.learn(total_timesteps=cfg.mode.total_timesteps)
 
     if cfg.mode.overwrite:
         model.save(current_model_file_path)
